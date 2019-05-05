4541830662084d50d6b43450397012f3163f599d
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-05-04 22:06:02 -0400

    create file to handle networking

---
+++ Foodie/Foodie/RecipeController.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeController.swift
+//  Foodie
+//
+//  Created by Julian A. Fordyce on 5/4/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import Foundation
+
+class RecipeController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
